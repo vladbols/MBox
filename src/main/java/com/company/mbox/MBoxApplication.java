956da454f641cadfaa609d0da8ff5ccedeeb9000
Rakhym Kurmangali <rakhym.kurmangali@uco.kz>
2022-06-19 15:50:26 +0600

    Database connection changed

--- src/main/java/com/company/mbox/MBoxApplication.java
+++ src/main/java/com/company/mbox/MBoxApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.event.EventListener;
@@ -16,7 +17,7 @@ import org.springframework.core.env.Environment;
 import javax.sql.DataSource;
 
 @SpringBootApplication
-public class MBoxApplication {
+public class MBoxApplication extends SpringBootServletInitializer {
 
     @Autowired
     private Environment environment;
